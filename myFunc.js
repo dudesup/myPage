0dc402b462af234fcb5e6b38430cc561c7ed6f3c
Daria Supińska <dariasupinska@gmail.com>
2020-01-26 12:17:16 +0100

    initial commit

---
+++ myFunc.js
@@ -0,0 +1,27 @@
+function myCV() {
+    document.getElementById("tytul").innerHTML= "CV";
+    document.getElementById("ind").innerHTML="Tu jest moje CV."
+}
+
+function myContact() {
+    document.getElementById("tytul").innerHTML= "Kontakt";
+    document.getElementById("ind").innerHTML="Tu jest mój mail, nr telefonu i inne<div class=\"contact\">" +
+        "<ul>" +
+        "<li>numer telefonu: 123 456 789</li>" +
+        "<li>mail: imie.nazwisko@gmail.com</li>" +
+        "<li>linkedIn: link@linkedin.com</li>" +
+        "</ul>" +
+        "</div>";
+}
+
+function whoAmI() {
+    document.getElementById("tytul").innerHTML= "O mnie";
+    document.getElementById("ind").innerHTML="Tu jest mój opis.";
+}
+
+function offer() {
+    document.getElementById("tytul").innerHTML= "Oferta";
+    document.getElementById("ind").innerHTML="Co oferuję?"
+}
+
+
